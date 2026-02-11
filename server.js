ef39a5bc846797f1c837ae4dfb7d0cbb0f1561b0
Vikash Kumar Kharwar <amit.sbz1596@gmail.com>
2026-02-11 18:58:18 +0530

    fixed routing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const connectDB = require("./config/db.config");
 const app = require("./app");
 
+
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
